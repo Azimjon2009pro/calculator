77deb40b887d2f76a38feb34c669e26a49c02fa0
Azimjon Sobirov <sobirovazimjon02@gmail.com>
2024-09-29 17:33:44 +0300

    Commit for Car class

--- main.py
+++ main.py
@@ -19,3 +19,30 @@ print(calc.add())
 print(calc.minus())
 print(calc.multiply())
 print(calc.divide())
+class Car:
+    def __init__(self, name: str,motor: str,model: str,speed: int,money: int):
+        self.name: str = name
+        self.motor: str = motor
+        self.model: str = model
+        self.speed: int = speed
+        self.money: int = money
+
+
+class Bmw(Car):
+    def __init__(self,name, motor, model, speed, money):
+        super().__init__(name,motor, model, speed, money)
+
+    def about_car(self):
+        return f"The cars name is {self.name}, and it's motor: {self.motor}, the maximum speed of car is {self.speed},anf finally car costs{self.money}"
+    
+class Mercesed(Car):
+    def __init__(self,name, motor, model, speed, money):
+        super().__init__(name,motor, model, speed, money)
+
+    def about_car(self):
+        return f"The cars name is {self.name}, and it's motor: {self.motor}, the maximum speed of car is {self.speed},anf finally car costs{self.money}"
+    
+bmw = Bmw("Bmw","Motor_Bmw123","M5 f90",2500,10000)
+print(bmw.about_car())
+merc = Mercesed("Mercedec","Motor_Benz233","Cls",2000,303444)
+print(merc.about_car())
